ad4627187687dd5df9a9df7b636d643e785e2796
coollogan876 <119338946+coollogan876@users.noreply.github.com>
2024-08-05 10:50:17 -0500

    Create site

---
+++ script.js
@@ -0,0 +1,3 @@
+alert("This page is meant for Anomalous Zone Staff ONLY\nYour IP address has been logged to ensure security")
+
+window.location.replace("https://youtu.be/dQw4w9WgXcQ?si=vxZuTfDdXcEI2L3q")
